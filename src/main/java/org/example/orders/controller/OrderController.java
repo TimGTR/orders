b0f6b8e9e13d6423f68571467661d4e6a924474d
Timur <tr.garipov@yandex.ru>
2024-12-11 18:35:55 +0300

    add stream logic

--- src/main/java/org/example/orders/controller/OrderController.java
+++ src/main/java/org/example/orders/controller/OrderController.java
@@ -3,13 +3,14 @@ package org.example.orders.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.orders.dto.Order;
-import org.example.orders.dto.OrderRequest;
 import org.example.orders.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -27,9 +28,15 @@ public class OrderController {
     }
 
     @GetMapping("/user/{userId}")
-    public Flux<Order> getOrdersByUserId(@PathVariable Long userId) {
+    public Flux<Order> getOrdersByUserId(@PathVariable Long userId) throws InterruptedException {
         log.info("Get orders by userId: {}", userId);
-        return orderService.getOrdersByUserId(userId);
+        return
+                Flux.interval(Duration.ofSeconds(1)) // Каждую секунду
+                        .map(i -> new Order(i.toString(), userId, "Item " + i, 1, 10.0 + i))
+                        .take(5);
+
+//                Mono.delay(Duration.ofSeconds(5))
+//                .flatMapMany(ignore -> Flux.just(new Order("0", 0L, "пк", 1, 1.0)));
     }
 }
 
